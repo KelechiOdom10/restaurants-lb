67de31f8899420a8c50d220fb40b65588c8c46f5
Kelechiodom10 <kelechiodom10@gmail.com>
2023-09-20 00:09:13 +0100

    chore: ensure export paths get updated

--- libs/ui/src/components/index.ts
+++ libs/ui/src/components/index.ts
@@ -1,11 +1,10 @@
 // @ts-nocheck
-export { default as Button } from "./Button.astro";
 export { default as Pill } from "./Pill.astro";
-export { default as ProductCard } from "./ProductCard.astro";
-export { default as ProductList } from "./ProductList.astro";
-export { default as CartIcon } from "./CartIcon.astro";
 export { default as SearchInput } from "./SearchInput.astro";
 export { default as CategoriesFilter } from "./CategoryFilter.astro";
+export { CartButton } from "./CartButton";
 export { CartDrawer } from "./CartDrawer";
-export { NumberInput } from "./NumberInput.tsx";
-export { OrderItem } from "./OrderItem.tsx";
+export { NumberInput } from "./NumberInput";
+export { OrderItem } from "./OrderItem";
+export { ProductCard } from "./ProductCard";
+export { ProductList } from "./ProductList";
