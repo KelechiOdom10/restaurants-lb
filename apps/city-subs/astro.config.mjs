893dd0f6690e3d06cc1ba0ea79fd6a3bbc801d25
Kelechiodom10 <kelechiodom10@gmail.com>
2023-09-20 00:08:33 +0100

    feat: use preact compat

--- apps/city-subs/astro.config.mjs
+++ apps/city-subs/astro.config.mjs
@@ -4,10 +4,5 @@ import preact from "@astrojs/preact";
 
 export default defineConfig({
   outDir: "../../dist/apps/city-subs",
-  integrations: [
-    tailwind({
-      config: "../city-subs/tailwind.config.js",
-    }),
-    preact(),
-  ],
+  integrations: [tailwind(), preact({ compat: true })],
 });
